ac5eee378ccd5675ff312294ad8586058fec5b9a
Kurt Feigl <feigl@wisc.edu>
2021-07-15 15:42:14 -0500

    Minor repairs

--- docker/setup_inside_container_gmtsar.sh
+++ docker/setup_inside_container_gmtsar.sh
@@ -18,7 +18,7 @@ fi
 export PATH=${HOME}/FringeFlow/sh:${PATH}
 export PATH=${HOME}/FringeFlow/docker:${PATH}
 export PATH=${HOME}/FringeFlow/gmtsar6:${PATH}
-export PATH=${HOME}/FringeFlow/gmtsar6/siteinfo:${PATH}
+export PATH=${HOME}/FringeFlow/siteinfo:${PATH}
 
 if [[ -d /home/ops/ssara_client ]]; then
     export PATH=${PATH}:/home/ops/ssara_client

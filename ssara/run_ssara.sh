f4594f2f56d3f0393004198d4270a5692afde0a4
Kurt Feigl <feigl@wisc.edu>
2021-07-15 15:11:16 -0500

    Minor repairs

--- ssara/run_ssara.sh
+++ ssara/run_ssara.sh
@@ -1,15 +1,27 @@
-#!/bin/bash -vex
+#!/bin/bash -e
 ## 2021/06/07 Kurt Feigl
 
 ## SSARA for downloading data
 
-if [ "$#" -ne 5 ]; then
+if [ "$#" -eq 5 ]; then
+    action='print'
+elif [ "$#" -eq 6 ]; then
+    action=$6
+else
     bname=`basename $0`
     echo "$bname will calculate an interferometric pair "
     echo "usage:   $bname SAT TRK SITE reference_YYYYMMDD secondary_YYYYMMDD"
     echo "example: $bname S1 144 SANEM 20190110  20190122"
-
     exit -1
+fi
+
+export SSARA_HOME=$( dirname $(which ssara_federated_query.py ) )
+echo "Checking for file named password_config.py in ${SSARA_HOME}"
+if [[ -f ${SSARA_HOME}/password_config.py ]]; then
+    ls -l ${SSARA_HOME}/password_config.py
+else
+   echo "ERROR: could not find ile named password_config.py in ${SSARA_HOME}"
+   exit -1
 fi
 
 #export YYYYMMDD1="2019-10-02"
@@ -33,7 +45,8 @@ echo trk is $trk
 echo sit is $sit
 echo t0 is $t0
 echo t1 is $t1
-
+timetag=`date +"%Y%m%dT%H%M%S"`
+echo timetag is ${timetag}
 
 # get working version of ssara client
 #cp -rp /home/feigl/SSARA-master $HOME
@@ -61,53 +74,33 @@ export LONMAX=`grep E tmp.wesn | awk '{print $3}'`
 export POLYGON="POLYGON(($LONMIN $LATMIN, $LONMAX $LATMIN, $LONMAX $LATMAX, $LONMIN $LATMAX, $LONMIN $LATMIN))"
 echo POLYGON is $POLYGON
 
-# ls -la password_config.py
-# ls -la $HOME/ssara_client/password_config.py
 
-# make list
-#ssara_federated_query.py --platform=SENTINEL-1A,SENTINEL-1B --asfResponseTimeout=30 --relativeOrbit=144 --intersectsWith='POINT(-119.3987026 40.37426071)' --start=${YYYYMMDD1} --end="${YYYYMMDD2} 23:59:59" --print | tee ssara.lst
-
-#  W =      -119.4600000000 
-#  E =      -119.3750000000
-#  S =        40.3480000000
-#  N =        40.4490000000
-#https://www.unavco.org/data/sar/sar_api/sar_api.html
-#intersectsWith — accepts a list of polygons, line segments ("linestring"), or a point defined in multipoint 2-D Well-Known Text (WKT); 
-#currently each polygon must be explicitly closed, i.e. the first vertex and the last vertex of each listed polygon must be identical; 
-#coordinate pairs for each vertex are in decimal degrees of longitude followed by decimal degree of latitude:
-#https://en.wikipedia.org/wiki/Well-known_text_representation_of_geometry
-#The OGC standard definition requires a polygon to be topologically closed. It also states that if the exterior linear ring of a polygon is defined in a counterclockwise direction
-
-#--intersectsWith='POLYGON((-119.4600000000 40.3480000000, -119.3750000000 40.3480000000, -119.3750000000 40.4490000000, -119.4600000000 40.4490000000, -119.4600000000 40.3480000000))' \
-
+echo "Starting query to print."
 ssara_federated_query.py --platform=SENTINEL-1A,SENTINEL-1B --asfResponseTimeout=30 --relativeOrbit=${trk} \
 --start=${YYYYMMDD1} --end=${YYYYMMDD2} \
 --intersectsWith="POLYGON(($LONMIN $LATMIN, $LONMAX $LATMIN, $LONMAX $LATMAX, $LONMIN $LATMAX, $LONMIN $LATMIN))" \
---print | tee ssara.lst
+--print | tee ssara_${timetag}.csv
+
+if [[ ! ${action} == "print" ]]; then
+
+    # make KML file
+    echo "Making KML file"
+    #ssara_federated_query.py --platform=SENTINEL-1A,SENTINEL-1B --asfResponseTimeout=30 --relativeOrbit=144 --intersectsWith='POINT(-119.3987026 40.37426071)' --start=${YYYYMMDD1} --end="${YYYYMMDD2} 23:59:59"  --kml
+    ssara_federated_query.py --platform=SENTINEL-1A,SENTINEL-1B --asfResponseTimeout=30 --relativeOrbit=${trk} \
+    --start=${YYYYMMDD1} --end=${YYYYMMDD2} \
+    --intersectsWith="POLYGON(($LONMIN $LATMIN, $LONMAX $LATMIN, $LONMAX $LATMAX, $LONMIN $LATMAX, $LONMIN $LATMIN))" \
+    --kml | tee ssara_${timetag}.kml
 
 
+    # download data
+    # switch for ICSE --start 2018-10-01 --stop 2018-11-15
+    #ssara_federated_query.py --platform=SENTINEL-1A,SENTINEL-1B --asfResponseTimeout=30 --relativeOrbit=144 --intersectsWith='POINT(-119.3987026 40.37426071)' --start=${YYYYMMDD1} --end="${YYYYMMDD2} 23:59:59" --download
 
-# make KML file
-#ssara_federated_query.py --platform=SENTINEL-1A,SENTINEL-1B --asfResponseTimeout=30 --relativeOrbit=144 --intersectsWith='POINT(-119.3987026 40.37426071)' --start=${YYYYMMDD1} --end="${YYYYMMDD2} 23:59:59"  --kml
-ssara_federated_query.py --platform=SENTINEL-1A,SENTINEL-1B --asfResponseTimeout=30 --relativeOrbit=${trk} \
---start=${YYYYMMDD1} --end=${YYYYMMDD2} \
---intersectsWith="POLYGON(($LONMIN $LATMIN, $LONMAX $LATMIN, $LONMAX $LATMAX, $LONMIN $LATMAX, $LONMIN $LATMIN))" \
---kml | tee ssara.kml
+    echo "Downloading data"
+    ssara_federated_query.py --platform=SENTINEL-1A,SENTINEL-1B --asfResponseTimeout=30 --relativeOrbit=${trk} \
+    --start=${YYYYMMDD1} --end=${YYYYMMDD2} \
+    --intersectsWith="POLYGON(($LONMIN $LATMIN, $LONMAX $LATMIN, $LONMAX $LATMAX, $LONMIN $LATMAX, $LONMIN $LATMIN))" \
+    --download | tee -a ssara_$timetag}.log
 
-
-# download data
-# end on the 19th, to get the 18th
-# python -m ssara_federated_query --platform=SENTINEL-1A,SENTINEL-1B --asfResponseTimeout=30 --relativeOrbit=144 --intersectsWith='POINT(-119.3987026 40.37426071)' --start=2018-10-06 --end=2018-10-19 --print
-# python -m ssara_federated_query --platform=SENTINEL-1A,SENTINEL-1B --asfResponseTimeout=30 --relativeOrbit=144 --intersectsWith='POINT(-119.3987026 40.37426071)' --start=2018-10-06 --end=2018-10-19 --download
-#python -m ssara_federated_query --platform=SENTINEL-1A,SENTINEL-1B --asfResponseTimeout=30 --relativeOrbit=144 --intersectsWith='POINT(-119.3987026 40.37426071)' --start=2019-10-06 --end=2018-10-19  --s1orbits --download
-#python -m ssara_federated_query --platform=SENTINEL-1A,SENTINEL-1B --asfResponseTimeout=30 --relativeOrbit=144 --intersectsWith='POINT(-119.3987026 40.37426071)' --start=2018-10-06 --end=2018-11-16 --download
-# switch for ICSE --start 2018-10-01 --stop 2018-11-15
-#ssara_federated_query.py --platform=SENTINEL-1A,SENTINEL-1B --asfResponseTimeout=30 --relativeOrbit=144 --intersectsWith='POINT(-119.3987026 40.37426071)' --start=${YYYYMMDD1} --end="${YYYYMMDD2} 23:59:59" --download
-
-ssara_federated_query.py --platform=SENTINEL-1A,SENTINEL-1B --asfResponseTimeout=30 --relativeOrbit=${trk} \
---start=${YYYYMMDD1} --end=${YYYYMMDD2} \
---intersectsWith="POLYGON(($LONMIN $LATMIN, $LONMAX $LATMIN, $LONMAX $LATMAX, $LONMIN $LATMAX, $LONMIN $LATMIN))" \
---download | tee -a ssara.log
-
-
-   
+fi
+  

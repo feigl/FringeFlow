b5044a1b11598a83932d749349f7799118b4f35a
Kurt Feigl <feigl@wisc.edu>
2021-11-05 10:02:31 -0500

    simplify to retrieve without deleting or plotting

--- gmtsar6/retrieve_pairs.sh
+++ gmtsar6/retrieve_pairs.sh
@@ -1,7 +1,9 @@
 #!/bin/bash -vx
 # retrieve pairs
 # 2021/07/09 Kurt Feigl
-
+# 2021/11/04 edit batzli Added some comments and commented-out line 101 for making UTMs since that is already done on submit-2. Changed mast-->ref and slav-->sec on line 56.
+# 2021/11/05 Kurt and Sam UTM files are already in tar ball, no need to make them here. Save plotting for later. 
+#   Retrieve, but do not delete tarball from /staging
 if [ "$#" -eq 2 ]; then
 	pairlist=${1}
     site=`echo ${2} | awk '{print tolower($1)}'`
@@ -40,19 +42,21 @@ echo "DATADIR is $DATADIR"
 #rsync --remove-source-files -rav ${ruser}@transfer.chtc.wisc.edu:/staging/groups/geoscience/insar/"In*" .
 rsync -rav ${ruser}@transfer.chtc.wisc.edu:/staging/groups/geoscience/insar/"In*" .
 
+# get and remove the log files. It would be better to pull these by names
+# rsync --remove-source-files -rav ${ruser}@submit-2.chtc.wisc.edu:"${SITE}*.log" .
+# rsync --remove-source-files -rav ${ruser}@submit-2.chtc.wisc.edu:"${SITE}*.out" .
+# rsync --remove-source-files -rav ${ruser}@submit-2.chtc.wisc.edu:"${SITE}*.err" .
 # get the log files. It would be better to pull these by names
-rsync --remove-source-files -rav ${ruser}@submit-2.chtc.wisc.edu:"${SITE}*.log" .
-rsync --remove-source-files -rav ${ruser}@submit-2.chtc.wisc.edu:"${SITE}*.out" .
-rsync --remove-source-files -rav ${ruser}@submit-2.chtc.wisc.edu:"${SITE}*.err" .
+rsync -rav ${ruser}@submit-2.chtc.wisc.edu:"${SITE}*.log" .
+rsync -rav ${ruser}@submit-2.chtc.wisc.edu:"${SITE}*.out" .
+rsync -rav ${ruser}@submit-2.chtc.wisc.edu:"${SITE}*.err" .
  
-
 #the following "while read" reads each line and all variables of the PAIRSmake.txt (not all present) to make the .sub file for each pair
-
 # a         b         c      d      e                    f                    g    h    i    j       k          l      m       n      o      p      q    r                       s
-# mast      slav      orb1   orb2   doy_mast             doy_slav             dt   nan  trk  orbdir  swath      site   wv      bpar   bperp  burst  sat  dem                     filter_wv   
+# ref       sec       orb1   orb2   doy_mast             doy_slav             dt   nan  trk  orbdir  swath      site   wv      bpar   bperp  burst  sat  dem                     filter_wv   
 # 20200415  20210505  54442  60287  105.054610604005006  124.054702444455998  385  NAN  T30  A       strip_004  forge  0.0311  -20.4  6.7    nan    TSX  forge_dem_3dep_10m.grd  80                      
 
-echo 'mast      slav      orb1   orb2   doy_ref             doy_sec             dt   nan  trk  orbdir  swath      site   wv      bpar   bperp  burst  sat  dem                     filter_wv' > goodpairs.txt 
+echo 'ref       sec       orb1   orb2   doy_ref             doy_sec             dt   nan  trk  orbdir  swath      site   wv      bpar   bperp  burst  sat  dem                     filter_wv' > goodpairs.txt 
 
 while read -r a b c d e f g h i j k l m n o p q r s; do
 # ignore commented lines
@@ -83,8 +87,12 @@ while read -r a b c d e f g h i j k l m n o p q r s; do
     #ssh ${ruser}@transfer.chtc.wisc.edu 'ls -l /staging/groups/geoscience/insar/In*.tgz'
     #ssh ${ruser}@transfer.chtc.wisc.edu "ls -l /staging/groups/geoscience/insar/In${ref}_${sec}.tgz"
 
+    # find retrieved tarballs, looking everywhere below PWD
+    #tgzs=`find . -name "In${ref}_${sec}*.tgz"`
+    # find retrieved tarballs, looking only inside PWD, i.e. don't try too hard
+    tgzs=`find . -maxdepth 1 -name "In${ref}_${sec}*.tgz"`
+
     # extract contents from tar files
-    tgzs=`find . -name "In${ref}_${sec}*.tgz"`
     if [[ ${#tgzs} -gt 0 ]]; then
         echo "output tar file tgzs is $tgzs"
         for tgz1 in $tgzs; do
@@ -96,27 +104,28 @@ while read -r a b c d e f g h i j k l m n o p q r s; do
         echo "Did not find an ouput tar file for In${ref}_${sec}"
     fi
 
-    # make UTM grids
-    prepare_grids_for_gipht6.sh $site
-
-    if [[ -d In${ref}_${sec} ]]; then
-        cd In${ref}_${sec}
-        if [[ -f phasefilt_mask_utm.grd ]]; then   
-            # make plot
-            # plot_pair6.sh TSX T30 forge forge_TSX_T30_strip004_20200324_20210311 phasefilt_mask_utm.grd phasefilt_mask_utm.ps 15.5 97.6 feigl 80 999. "dem" $PWD
-            # echo "plot_pair.sh $sat $trk $site $pair $pair/${pha1}.grd ${pair}_${pha1}.ps $mmperfringe $bperp $user $filter_wv $dt $demf"
-            # plot_pair.sh $sat $trk $site $pair $pair/${pha1}.grd ${pair}_${pha1}.ps $mmperfringe $bperp $user $filter_wv $dt $demf
-            #plot_pair6.sh  TSX T30 forge "title" phasefilt_mask_utm.grd phase_filt_mask.ps 15.5 63.2 $USER 80 999 In20181115_20190418
-            plot_pair6.sh  $sat $trk $site $pairdir phasefilt_mask_utm.grd phasefilt_mask_utm.ps $mmperfringe $bperp $user $filter_wv $dt $demf
+    ## make UTM grids - already done upstream 
+    # prepare_grids_for_gipht6.sh $site
+   
+    ## make plots - no need now
+    # if [[ -d In${ref}_${sec} ]]; then
+    #     cd In${ref}_${sec}
+    #     if [[ -f phasefilt_mask_utm.grd ]]; then   
+    #         # make plot
+    #         # plot_pair6.sh TSX T30 forge forge_TSX_T30_strip004_20200324_20210311 phasefilt_mask_utm.grd phasefilt_mask_utm.ps 15.5 97.6 feigl 80 999. "dem" $PWD
+    #         # echo "plot_pair.sh $sat $trk $site $pair $pair/${pha1}.grd ${pair}_${pha1}.ps $mmperfringe $bperp $user $filter_wv $dt $demf"
+    #         # plot_pair.sh $sat $trk $site $pair $pair/${pha1}.grd ${pair}_${pha1}.ps $mmperfringe $bperp $user $filter_wv $dt $demf
+    #         #plot_pair6.sh  TSX T30 forge "title" phasefilt_mask_utm.grd phase_filt_mask.ps 15.5 63.2 $USER 80 999 In20181115_20190418
+    #         plot_pair6.sh  $sat $trk $site $pairdir phasefilt_mask_utm.grd phasefilt_mask_utm.ps $mmperfringe $bperp $user $filter_wv $dt $demf
             
-            # make an exceptionally well documented CSV file;-)
-            gmt grdinfo phasefilt_mask_utm.grd     | awk '{print "#",$0}' > phasefilt_mask_utm.csv
-            gmt grd2xyz -s -fo phasefilt_mask_utm.grd   | awk '{printf("%.0f,%.0f,%.3f\n",$1,$2,$3)}' >> phasefilt_mask_utm.csv
+    #         # make an exceptionally well documented CSV file;-)
+    #         gmt grdinfo phasefilt_mask_utm.grd     | awk '{print "#",$0}' > phasefilt_mask_utm.csv
+    #         gmt grd2xyz -s -fo phasefilt_mask_utm.grd   | awk '{printf("%.0f,%.0f,%.3f\n",$1,$2,$3)}' >> phasefilt_mask_utm.csv
 
-            echo $a $b $c $d $e $f $g $h $i $j $k $l $m $n $o $p $q $r $s >> ../goodpairs.txt 
-        fi
-        cd ..
-    fi
+    #         echo $a $b $c $d $e $f $g $h $i $j $k $l $m $n $o $p $q $r $s >> ../goodpairs.txt 
+    #     fi
+    #     cd ..
+    # fi
 
 done < ${pairlist}   # end of "while read" loop from above
 

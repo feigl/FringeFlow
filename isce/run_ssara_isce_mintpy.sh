5c4b5e9976b87f770ccbfe7553400b3fbc219cc9
feigl <feigl@wisc.edu>
2022-12-15 15:55:32 -0600

    save _condor_stdout and _condor_stderr

--- isce/run_ssara_isce_mintpy.sh
+++ isce/run_ssara_isce_mintpy.sh
@@ -229,7 +229,7 @@ pushd $WORKDIR/$RUNNAME # I think we should already be there, but just in case
 # 2022/08/08 Kurt - add folders only
 
 if [[  -d /staging/groups/geoscience ]]; then
-    tar -czf "$RUNNAME.tgz" DEM ORBITS ISCE/reference ISCE/baselines ISCE/merged ISCE/geom_reference MINTPY
+    tar -czf "$RUNNAME.tgz" DEM ORBITS ISCE/reference ISCE/baselines ISCE/merged ISCE/geom_reference MINTPY _condor_stdout _condor_stderr
     mkdir -p "/staging/groups/geoscience/isce/output/"
     cp -fv "$RUNNAME.tgz" "/staging/groups/geoscience/isce/output/$RUNNAME.tgz"
     # delete working dir contents to avoid transfering files back to /home/ on submit2
